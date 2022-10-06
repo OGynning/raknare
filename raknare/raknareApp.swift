8cd180a26589274fe33fb6f8ee83c2925295dc8f
Oscar Gynning <oscar.gynning@gmail.com>
2022-10-06 13:01:18 +0200

    Initial Commit

---
+++ raknare/raknareApp.swift
@@ -0,0 +1,17 @@
+//
+//  raknareApp.swift
+//  raknare
+//
+//  Created by oscar gynning on 2022-10-06.
+//
+
+import SwiftUI
+
+@main
+struct raknareApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
